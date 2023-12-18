1dfb919584f7ebc270e4765af5992e57182a1698
Enes <Enes@ENES>
2023-12-18 14:40:38 +0300

    Add project files.

---
+++ BankCustomerFollow/CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankCustomerFollow
+{
+    internal class CustomerManager
+
+
+    {
+        public void Add(Customer customer) {
+            Console.WriteLine("customer informations added");
+        }
+
+        public void Remove(Customer Customer)
+        {
+            Console.WriteLine("Customer informations removed");
+        }
+
+    }
+}
---
+++ BankCustomerFollow/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BankCustomerFollow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name =" enes yaprak";
+            customer.Id = 001;
+            customer.AccountBalance = 10000;
+            
+            Customer customer1 = new Customer();
+            customer1.Name = "Engin Demiroğ";
+            customer1.Id = 002;
+            customer1.AccountBalance = 5000000;
+        
+
+        CustomerManager customermanager = new CustomerManager();
+
+            customermanager.Add(customer);
+            customermanager.Remove(customer);
+
+            customermanager.Add(customer1);
+            customermanager.Remove(customer1);
+
+
+
+
+        }
+    }
+}
